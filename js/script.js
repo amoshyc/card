82626f7f54ca49a6faae9aa95c641bb7152e838a
amoshyc <amoshuangyc@gmail.com>
2017-02-09 15:12:45 +0800

    Change layout of article and Modify nav-button

--- js/script.js
+++ js/script.js
@@ -7,15 +7,39 @@ function init_nav_button() {
     if (btnUp == null || btnDown == null || content == null || main == null)
         return;
 
+    // go top button
     btnUp.click(function() {
         content.animate({
-          scrollTop: 0
-        }, 200);
+            scrollTop: 0
+        }, 200, 'swing', function() {
+            window.location.hash = "";
+        });
     });
+
+    // next card
+    var anchors = $('a.anchor');
+    var current = content.scrollTop();
+    var ids = [], offsets = [];
+    anchors.each(function(idx, val) {
+        var elem = $(val);
+        ids.push(elem.attr('id'));
+        offsets.push(current + elem.offset().top);
+    });
+
     btnDown.click(function() {
-        content.animate({
-          scrollTop: main.height()
-        }, 200);
+        var cur = content.scrollTop();
+        for (idx in offsets) {
+            if (offsets[idx] > cur + 1) { // prevent eps
+                var id = ids[idx];
+                var target = offsets[idx];
+                content.animate({
+                    scrollTop: target
+                }, 200, 'swing', function() {
+                    window.location.hash = id;
+                });
+                break;
+            }
+        }
     });
 }
 
